88a2244493b41cf0a1833c5a4aa8c526e085c35c
lukasz <lukasz.sulka@wp.pl>
2023-10-07 22:09:25 +0200

    zadanko

--- zadanie.py
+++ zadanie.py
@@ -2,11 +2,12 @@ import random
 
 wprowadzona = input(f"podaj liczbę z zakresu 0-100: ")
 num = random.randrange(0, 5)
-
+liczba_prob = 1
 while wprowadzona != num:
     print('try again')
     wprowadzona = int(input(f"podaj liczbę z zakresu 0-100: "))
+    liczba_prob = liczba_prob + 1
 while wprowadzona == num:
     print(f'odgadles, twoja liczba to {num}')
     break
-
+print(f'zajęło ci to {liczba_prob} razy')
