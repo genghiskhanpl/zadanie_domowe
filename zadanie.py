88fd408d4779dfdfa01ebe4b854fd97766ed7f95
lukasz <lukasz.sulka@wp.pl>
2023-10-07 21:00:23 +0200

    zadanko

--- zadanie.py
+++ zadanie.py
@@ -5,5 +5,9 @@ losowana_liczba = random.randrange(0, 100)
 print(f'wprowadziłeś liczbę: {wprowadzona}')
 print(f'wylosowana liczba to {losowana_liczba}')
 
-
-
+if wprowadzona == losowana_liczba:
+    print('gratulacje odgadłeś')
+else:
+    print ('sprobuj ponownie')
+    wprowadzona = int(input(f"podaj liczbę z zakresu 0-100: "))
+# chyba musze sie nauczyć pętli
