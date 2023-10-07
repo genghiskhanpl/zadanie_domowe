491e29aa8ba52a0197df04b361aaeda001fea2b8
lukasz <lukasz.sulka@wp.pl>
2023-10-07 20:47:23 +0200

    zadanko

---
+++ zadanie.py
@@ -0,0 +1,9 @@
+import random
+
+wprowadzona = int(input(f"podaj liczbę z zakresu 0-100: "))
+losowana_liczba = random.randrange(0, 100)
+print(f'wprowadziłeś liczbę: {wprowadzona}')
+print(f'wylosowana liczba to {losowana_liczba}')
+
+
+
