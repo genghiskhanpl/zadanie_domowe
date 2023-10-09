221939ebac2a31331872986843d4e15cddca7196
genghiskhanpl <lukasz.sulka@wp.pl>
2023-10-09 15:25:05 +0200

    zadanko odrobione i dodane podpowiedzi - większa, mniejsza

--- zadanie.py
+++ zadanie.py
@@ -1,21 +1,32 @@
 import random, sys
 
-name = input('Wprowadź nazwę użytkownika: ')
-if len(name) < 3:
-    print('wprowadz imię, minimum 3 znaki')
-    sys.exit('konczenie programu')
+while True:
+    name = input('Wprowadź nazwę użytkownika: ')
+    if len(name) < 3:
+        print('wprowadz imię, minimum 3 znaki')
+        sys.exit('konczenie programu')
 
-wprowadzona = int(input(f"{name} podaj liczbę z zakresu 0-100: "))
-num = random.randrange(1, 100)
-liczba_prob = 1
-while wprowadzona != num:
-    if wprowadzona >= num:
-        print('Wprowadzona liczba była większa niż losowana')
-    elif wprowadzona <= num:
-        print('Wprowadzona liczba była mniejsza niż losowana')
-    wprowadzona = int(input(f"Wpisz liczbę z zakresu 0-100: "))
-    liczba_prob = liczba_prob + 1
-while wprowadzona == num:
-    print(f'Gratulacje {name} odgadles, Twoja liczba to {num}')
-    break
-print(f'Zajęło ci to {liczba_prob} razy')
+    wprowadzona = int(input(f"{name} podaj liczbę z zakresu 0-100: "))
+    num = random.randrange(1, 100)
+    liczba_prob = 1
+    while wprowadzona != num:
+        if wprowadzona >= num:
+            print('Wprowadzona liczba była większa niż losowana')
+        elif wprowadzona <= num:
+            print('Wprowadzona liczba była mniejsza niż losowana')
+        wprowadzona = int(input(f"Wpisz liczbę z zakresu 0-100: "))
+        liczba_prob = liczba_prob + 1
+    while wprowadzona == num:
+        print(f'Gratulacje {name} odgadles, Twoja liczba to {num}')
+        break
+    print(f'Zajęło ci to {liczba_prob} razy')
+    while True:
+        odpowiedz = str(input("Zagramy jeszcze? (t/n)?"))
+        if odpowiedz in ('t','n'):
+            break
+        print ('zła odp')
+        if odpowiedz == 't':
+            continue
+        else:
+            print("na razie!")
+            break
