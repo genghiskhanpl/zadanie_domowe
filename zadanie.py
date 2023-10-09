7fce459e6099cef6f6a75a791829e5075778d1e8
lukasz <lukasz.sulka@wp.pl>
2023-10-09 12:17:46 +0200

    zadanko odrobione i dodane podpowiedzi - większa, mniejsza

--- zadanie.py
+++ zadanie.py
@@ -1,10 +1,13 @@
 import random
 
-wprowadzona = input(f"podaj liczbę z zakresu 0-100: ")
+wprowadzona = int(input(f"podaj liczbę z zakresu 0-100: "))
 num = random.randrange(1, 100)
 liczba_prob = 1
 while wprowadzona != num:
-    print('try again')
+    if wprowadzona >= num:
+        print('wprowadzona liczba była większa niż losowana')
+    elif wprowadzona <= num:
+        print('wprowadzona liczba była mniejsza niż losowana')
     wprowadzona = int(input(f"podaj liczbę z zakresu 0-100: "))
     liczba_prob = liczba_prob + 1
 while wprowadzona == num:
