897a660843006700ffb7ba9223aab7816703de02
genghiskhanpl <lukasz.sulka@wp.pl>
2023-10-10 10:36:35 +0200

    zadanko odrobione i dodane podpowiedzi - większa, mniejsza

--- zadanie.py
+++ zadanie.py
@@ -1,4 +1,5 @@
 import random, sys
+gramy_ponownie = ''
 
 while True:
     name = input('Wprowadź nazwę użytkownika: ')
@@ -7,26 +8,28 @@ while True:
         sys.exit('konczenie programu')
 
     wprowadzona = int(input(f"{name} podaj liczbę z zakresu 0-100: "))
-    num = random.randrange(1, 100)
+    num = random.randrange(1, 5)
     liczba_prob = 1
     while wprowadzona != num:
         if wprowadzona >= num:
             print('Wprowadzona liczba była większa niż losowana')
         elif wprowadzona <= num:
             print('Wprowadzona liczba była mniejsza niż losowana')
-        wprowadzona = int(input(f"Wpisz liczbę z zakresu 0-100: "))
+        wprowadzona = int(input(f'Wpisz liczbę z zakresu 0-100: '))
         liczba_prob = liczba_prob + 1
     while wprowadzona == num:
         print(f'Gratulacje {name} odgadles, Twoja liczba to {num}')
         break
     print(f'Zajęło ci to {liczba_prob} razy')
-    while True:
-        odpowiedz = str(input("Zagramy jeszcze? (t/n)?"))
-        if odpowiedz in ('t','n'):
-            break
-        print ('zła odp')
-        if odpowiedz == 't':
-            continue
-        else:
-            print("na razie!")
-            break
+    gramy_ponownie = str(input("czy gramy jeszcze raz? (t/n)?"))
+
+    if gramy_ponownie == 't':
+        print('zagrajmy')
+        continue
+    elif gramy_ponownie == 'n':
+        print('na razie')
+        sys. exit()
+    else:
+        print('wpisz "t" lub "n": ')
+#czesc ktora pyta czy zagramy jeszcze raz
+
