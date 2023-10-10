6cdd6c1e8788b03824009cd5b8c50f0efbe0bee5
genghiskhanpl <lukasz.sulka@wp.pl>
2023-10-10 12:52:40 +0200

    zadanko odrobione i dodane podpowiedzi - większa, mniejsza

---
+++ whilecwiczenia.py
@@ -0,0 +1,7 @@
+while True:
+    num = int(input('podaj liczbe wieksza od 0: '))
+    if num > 0:
+        print(f'Twoja liczba to {num}')
+        break
+    else:
+        print('to nie jest prawidłowa liczba')
